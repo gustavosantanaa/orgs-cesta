57431b6b1b46220484f6cac54669e91aede7ad2a
Gustavo Cardoso Santana <gustavosantanaa02@gmail.com>
2023-11-02 10:29:10 -0300

    :construction: base do app orgs cesta

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { StatusBar, SafeAreaView } from 'react-native';
+import Cesta from './src/telas/cesta';
+
+export default function App() {
+  return (
+    <SafeAreaView>
+      <StatusBar />
+      <Cesta />
+    </SafeAreaView>
+  );
+}
